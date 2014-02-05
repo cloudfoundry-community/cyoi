18a269f35e6651403fb57b2aabb471ae613c74f0
Dr Nic Williams <drnicwilliams@gmail.com>
2014-02-05 16:53:55 -0500

    spec for provision_public_ip_address for nova

--- spec/unit/providers/clients/openstack_provider_client_spec.rb
+++ spec/unit/providers/clients/openstack_provider_client_spec.rb
@@ -1,4 +1,5 @@
 require "fog"
+require "fog/openstack/models/compute/addresses"
 require "cyoi/providers"
 
 describe "cyoi address openstack" do
@@ -17,9 +18,23 @@ describe "cyoi address openstack" do
     }
   end
 
+  let(:fog_compute) { instance_double("Fog::Compute::OpenStack::Real") }
+  let(:addresses) { instance_double("Fog::Compute::OpenStack::Addresses") }
+  let(:address) { instance_double("Fog::Compute::OpenStack::Address", ip: '1.2.3.4') }
+
   subject { Cyoi::Providers.provider_client(provider_attributes) }
 
   it "is an OpenStack provider" do
     expect(subject).to be_instance_of(Cyoi::Providers::Clients::OpenStackProviderClient)
+  end
+
+  context "provision_public_ip_address" do
+    it "defaults to pool: nil for nova" do
+      expect(subject).to receive(:fog_compute).and_return(fog_compute)
+      expect(fog_compute).to receive(:addresses).and_return(addresses)
+      expect(addresses).to receive(:get_address_pools).and_return([{"name"=>"nova"}])
+      expect(addresses).to receive(:create).with(pool: "nova").and_return(address)
+      expect(subject.provision_public_ip_address).to eq('1.2.3.4')
+    end
   end
 end
