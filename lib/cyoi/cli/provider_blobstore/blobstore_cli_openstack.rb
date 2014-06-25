e822dba2338a94331c586db01293630cd5dfcd97
Dr Nic Williams <drnicwilliams@gmail.com>
2014-06-25 16:29:28 -0700

    fix loading of openstack blobstore client

--- lib/cyoi/cli/provider_blobstore/blobstore_cli_openstack.rb
+++ lib/cyoi/cli/provider_blobstore/blobstore_cli_openstack.rb
@@ -2,4 +2,4 @@ require "cyoi/cli/provider_blobstore/blobstore_cli_base"
 class Cyoi::Cli::Blobstore::BlobstoreCliOpenStack < Cyoi::Cli::Blobstore::BlobstoreCliBase
 end
 
-Cyoi::Cli::Blobstore.register_cli("aws", Cyoi::Cli::Blobstore::BlobstoreCliOpenStack)
+Cyoi::Cli::Blobstore.register_cli("openstack", Cyoi::Cli::Blobstore::BlobstoreCliOpenStack)
